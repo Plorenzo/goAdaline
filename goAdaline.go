3324b21d5a3b0577cb96647854d98cafa04c032b
Pablo Lorenzo <palelorenzo_7@hotmail.com>
2017-09-09 16:32:31 +0200

    Improved csv imported, added random weights generator

--- goAdaline.go
+++ goAdaline.go
@@ -1,34 +1,52 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"os"
+	"encoding/csv"
+	"math/rand"
+	"strconv"
+	"reflect"
 )
 
 const path = "/Users/plorenzo/dev/uni/rna/p1"
 
-func readCSV(filepath string) [][]string {
+func readCSV(filepath string) [][]float64 {
 
 	csvfile, err := os.Open(filepath)
-
 	if err != nil {
 		return nil
 	}
 
 	reader := csv.NewReader(csvfile)
-	fields, err := reader.ReadAll()
+	stringMatrix, err := reader.ReadAll()
 
 	csvfile.Close()
 
-	return fields
+	matrix := make([][]float64, len(stringMatrix))
+
+	for i := range stringMatrix {
+		matrix[i] = make([]float64, len(stringMatrix[0]))
+		for y := range stringMatrix[i] {
+			matrix[i][y], err = strconv.ParseFloat(stringMatrix[i][y], 64)
+		}
+	}
+
+	return matrix
+}
+
+func initWeights(length int) []float64 {
+
+	weights := make([]float64, length)
+	for index := range weights {
+		weights[index] = rand.Float64()
+	}
+
+	return weights
 }
 
 func main() {
 
 	data := readCSV(path)
-	fmt.Println(data[0][0])
-
-	
 
 }
